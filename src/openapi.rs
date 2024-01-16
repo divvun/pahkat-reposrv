06fcd5d47454e15e3c1bd6dac68f019ff75bcbdc
Dylan Hand <dylan.hand@gmail.com>
2024-01-16 01:48:48 +0100

    Accept name and description maps for updating metadata (#2)
    * Fix warning
    
    * This updated itself
    
    * Accept LangTagMap for name and description
    
    * Name and description can be updated on package update request

--- src/openapi.rs
+++ src/openapi.rs
@@ -43,6 +43,8 @@ struct UpdatePackageMetadataResponse {
 
 #[derive(Object, Debug, Clone)]
 pub struct UpdatePackageMetadataRequest {
+    pub name: Option<pahkat_types::LangTagMap<String>>,
+    pub description: Option<pahkat_types::LangTagMap<String>>,
     pub version: String,
     pub channel: Option<String>,
     #[oai(default)]
@@ -54,8 +56,8 @@ pub struct UpdatePackageMetadataRequest {
 
 #[derive(Object, Debug, Clone)]
 pub struct CreatePackageMetadataRequest {
-    pub name: String,
-    pub description: String,
+    pub name: pahkat_types::LangTagMap<String>,
+    pub description: pahkat_types::LangTagMap<String>,
     pub tags: Vec<String>,
 }
 
@@ -124,8 +126,10 @@ fn modify_repo_metadata(
     };
 
     let inner_req = package::update::Request::builder()
-        .repo_path(path.clone().into())
-        .id(package_id.clone().into())
+        .repo_path(path.into())
+        .id(package_id.into())
+        .name(release.name.as_ref().map(|x| Cow::Borrowed(&*x)))
+        .description(release.description.as_ref().map(|x| Cow::Borrowed(&*x)))
         .version(Cow::Owned(version))
         .channel(release.channel.as_ref().map(|x| Cow::Borrowed(&**x)))
         .target(Cow::Borrowed(&release.target))
